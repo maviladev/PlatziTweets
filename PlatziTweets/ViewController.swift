26d4dfaae5cd004f45fbfaf4a69d01bc73cdb484
Marcos Avila <marcoa31@gmail.com>
2020-08-26 05:31:13 -0400

    Initial Commit

---
+++ PlatziTweets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatziTweets
+//
+//  Created by XCodeClub on 2020-08-26.
+//  Copyright © 2020 avilamisc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
