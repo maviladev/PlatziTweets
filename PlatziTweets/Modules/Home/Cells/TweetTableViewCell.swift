fb15f4c779567caff37a9e1b8d8da17079d3f4b3
Marcos Avila <marcoa31@gmail.com>
2020-09-03 07:10:06 -0400

    Correccion de vista registro. Se creo diseño de tweet.

---
+++ PlatziTweets/Modules/Home/Cells/TweetTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  TweetTableViewCell.swift
+//  PlatziTweets
+//
+//  Created by XCodeClub on 2020-09-03.
+//  Copyright © 2020 avilamisc. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class TweetTableViewCell: UITableViewCell {
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var tweetImageView: UIImageView!
+    @IBOutlet weak var videoButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setupCellWith(post: PostItem) {
+        nameLabel.text = post.author.names
+        nicknameLabel.text = post.author.nickname
+        messageLabel.text = post.text
+        
+        if post.hasImage {
+            tweetImageView.kf.setImage(with: URL(string: post.imageUrl!))
+        } else {
+            tweetImageView.isHidden = true
+        }
+    }
+}
---
+++ PlatziTweets/Modules/Home/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  PlatziTweets
+//
+//  Created by XCodeClub on 2020-09-03.
+//  Copyright © 2020 avilamisc. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - IBOulets
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
