91a6b6f9593588f0b4bb3338cb59dd6546485249
Marcos Avila <marcoa31@gmail.com>
2020-09-05 16:34:56 -0400

    Diseño pantalla modal para publicar tweet

---
+++ PlatziTweets/Modules/Home/AddPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddPostViewController.swift
+//  PlatziTweets
+//
+//  Created by XCodeClub on 2020-09-05.
+//  Copyright © 2020 avilamisc. All rights reserved.
+//
+
+import UIKit
+
+class AddPostViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var postTextView: UITextView!
+    
+    // MARK: - IBActions
+    @IBAction func addPostAction(){
+        
+    }
+    @IBAction func dismissAction(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
